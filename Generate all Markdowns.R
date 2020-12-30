6b6ac8e7c091dbec87206ae032cbd6d453a62bbd
lirabenjamin <lira.benjamin@gmail.com>
2020-12-30 16:31:00 -0500

    Fixed minor issues
    Changed location image, formatting, moodmeter, etc.

--- Generate all Markdowns.R
+++ Generate all Markdowns.R
@@ -6,14 +6,15 @@ library(tidyverse)
 
 #Read all data
 fulldata <- readRDS("~/Documents/GitHub/Pollev-Personalized-Reports/pollev_wide.rds")
-
+fulldata = read_csv("Data with Moodmeter.csv")
+fulldata = fulldata %>% mutate(png = paste0(No,".png"))
 i = 1
+
 #Loop through people
 for (j in 1:nrow(fulldata)){
   data = slice(fulldata,j)
   write_rds(data,file = "~/Documents/GitHub/Pollev-Personalized-Reports/pollev_wide_i.rds")
-  path = paste0("Moodmeter/",j,".pdf")
-  rmarkdown::render('UntitledPres.Rmd',output_file = paste0("Markdowns/",j,". Grit Lab Report ",data$firstname))
+  path = paste0("Moodmeter/",data$png[1])
+  rmarkdown::render('UntitledPres.Rmd',
+                    output_file = paste0("Markdowns/",j,". Grit Lab Report ",data$firstname))
 }
-
-
